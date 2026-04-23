9d7cc6e75b249c858d5083fc27ce0de2663e1c29
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-04-23 14:37:15 +0530

    intital

--- counter-project/src/App.jsx
+++ counter-project/src/App.jsx
@@ -1,25 +1,40 @@
 import { useState } from 'react'
-import './App.css'
 
 
-let App = () =>{
-  const [counter , setCounter] = useState(0)
+
+let App = () => {
+  const [counter, setCounter] = useState(0)
+  // const addvalue = () => {
+  //   if (counter < 20) {
+  //     setCounter(counter + 1)
+  //   }
+
+  // }
+  // const decreasevalue = () => {
+  //   if (counter > 0) {
+  //     setCounter(counter - 1)
+  //   }
+
+  let maxval = 20
+
+  let minval = 0
+
   const addvalue = () => {
+    if(counter < maxval)
     setCounter(counter + 1)
   }
   const decreasevalue = () => {
-    if(counter > 0) {
-      setCounter(counter - 1)
+    if(counter > minval){
+    setCounter(counter - 1)
     }
-   
   }
-return (
+  return (
     <>
-    <h1>Counter</h1>
-    
-    <p>Counter Value : {counter}</p>
-    <button onClick={addvalue}>+ Add Value</button>
-    <button onClick={decreasevalue}>- Decrease Value</button>
+      <h1>Counter</h1>
+
+      <p>Counter Value : {counter}</p>
+      <button onClick={addvalue}>+ Add Value</button>
+      <button onClick={decreasevalue}>- Decrease Value</button>
     </>
   )
 }
